728dad8512e9b6b26abcf341044ca5442cfaae57
dantin <chengjie.ding@gmail.com>
2017-03-22 11:33:07 +0800

    add assembly descriptor file

--- kafka-producer/src/main/java/com/cosmos/kafka/client/producer/SimpleProducer.java
+++ kafka-producer/src/main/java/com/cosmos/kafka/client/producer/SimpleProducer.java
@@ -6,19 +6,18 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
 
-/**
- * Created by david on 21/03/2017.
- */
 public class SimpleProducer {
     private static KafkaProducer<Integer, String> producer;
 
     public SimpleProducer() {
         final Properties props = new Properties();
-        props.put("broker.list", "localhost:9092");
-        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("metadata.broker.list", "localhost:9092");
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("request.required.acks", "1");
 
-        producer = new KafkaProducer<Integer, String>(props);
+        producer = new KafkaProducer<>(props);
     }
 
     public static void main(String[] args) {
@@ -26,7 +25,7 @@ public class SimpleProducer {
         String topic = args[0];
         String message = args[1];
         System.out.printf("Send: %s\n", message);
-        producer.send(new ProducerRecord<Integer, String>(topic, message));
+        producer.send(new ProducerRecord<>(topic, message));
         producer.close();
     }
 }
