1c2d891e8628adaf7fdff356bf23feb102c2ad1e
dantin <chengjie.ding@gmail.com>
2017-03-24 17:04:00 +0800

    change key/value serializer

--- kafka-producer/src/main/java/com/cosmos/kafka/client/producer/MultipleBrokerProducer.java
+++ kafka-producer/src/main/java/com/cosmos/kafka/client/producer/MultipleBrokerProducer.java
@@ -19,8 +19,8 @@ public class MultipleBrokerProducer implements Runnable {
     public MultipleBrokerProducer(String topic) {
         final Properties props = new Properties();
         props.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
-        props.put(KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer");
+        props.put(VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer");
         props.put(PARTITIONER_CLASS_CONFIG, "org.apache.kafka.clients.producer.internals.DefaultPartitioner");
         props.put(ACKS_CONFIG, "1");
 
