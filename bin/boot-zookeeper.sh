6d212a1b9b0f1cb39cc5afee048d207ad274d938
dantin <chengjie.ding@gmail.com>
2017-03-24 18:14:18 +0800

    fix filename typo error

--- bin/boot-zookeeper.sh
+++ bin/boot-zookeeper.sh
@@ -2,5 +2,5 @@
 
 KAFKA_ROOT=/opt/kafka/kafka_2.12-0.10.2.0
 
-${KAFKA_ROOT}/bin/zookeeper-server-start.sh ${KAFKA_ROOT}/config/zookeepr.properties
+${KAFKA_ROOT}/bin/zookeeper-server-start.sh ${KAFKA_ROOT}/config/zookeeper.properties
 
