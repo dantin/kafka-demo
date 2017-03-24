eb40474550076a9fbfd55127d2b3ccb3ef8bb0db
dantin <chengjie.ding@gmail.com>
2017-03-24 18:11:25 +0800

    fix typo-error

--- bin/boot-zookeeper.sh
+++ bin/boot-zookeeper.sh
@@ -2,4 +2,5 @@
 
 KAFKA_ROOT=/opt/kafka/kafka_2.12-0.10.2.0
 
-${KAFKA_ROOT}/bin/zookeeper-server-start.sh ${KAFKA_ROOT}/config/zookper.properties
+${KAFKA_ROOT}/bin/zookeeper-server-start.sh ${KAFKA_ROOT}/config/zookeepr.properties
+
