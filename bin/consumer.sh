093b0c6a57b66a3aa8d11ff5ba03fddf096b5bae
dantin <chengjie.ding@gmail.com>
2017-03-28 16:05:34 +0800

    fix scripts

--- bin/consumer.sh
+++ bin/consumer.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-MODULE_NAME=kafka-consumer
-
-cd `dirname $0`
-BIN_DIR=`pwd`
-cd ..
-DEPLOY_DIR=`pwd`
-
-unzip ${DEPLOY_DIR}/${MODULE_NAME}/target/${MODULE_NAME}-1.0-SNAPSHOT-bin.zip -d ${DEPLOY_DIR}/${MODULE_NAME}/target
-
-LIB_DIR=${DEPLOY_DIR}/${MODULE_NAME}/target/lib
-LIB_JARS=`ls ${LIB_DIR} | grep .jar | awk '{print "'${LIB_DIR}'/"$0}' | tr "\n" ":"`
-
-java -classpath ${LIB_JARS} com.cosmos.kafka.client.Main kafkatopic
+java -jar kafka-consumer/build/libs/kafka-consumer-1.0-SNAPSHOT.jar simple kafkatopic
--- bin/producer.sh
+++ bin/producer.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-MODULE_NAME=kafka-producer
-
-cd `dirname $0`
-BIN_DIR=`pwd`
-cd ..
-DEPLOY_DIR=`pwd`
-
-unzip ${DEPLOY_DIR}/${MODULE_NAME}/target/${MODULE_NAME}-1.0-SNAPSHOT-bin.zip -d ${DEPLOY_DIR}/${MODULE_NAME}/target
-
-LIB_DIR=${DEPLOY_DIR}/${MODULE_NAME}/target/lib
-LIB_JARS=`ls ${LIB_DIR} | grep .jar | awk '{print "'${LIB_DIR}'/"$0}' | tr "\n" ":"`
-
-java -classpath ${LIB_JARS} com.cosmos.kafka.client.Simple kafkatopic
+java -jar kafka-producer/build/libs/kafka-producer-1.0-SNAPSHOT.jar simple kafkatopic
